f2e2ce998c3a4a70e62a54386b69657a534e430d
chandanthakurkgp-code <chandan.thakurkgp@gmail.com>
2026-01-20 02:07:24 +0530

    Add files via upload

---
+++ sdpoData.js
@@ -0,0 +1,12 @@
+const sdpoData = {
+    "sdpo": {
+        "title": "sdpo",
+        "icon": "✂️",
+        "rates": ["Haircut: ₹100", "Shave: ₹60", "Facial: ₹500"],
+        "partners": [
+            { "name": "आर्यन हेयर सैलून", "address": "गोल बाज़ार", "mobile": "9800012301", "whatsapp": "919800012301", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz1" },
+            { "name": "SI Purushottam Pandey", "address": "I/C Kharagpur Town PS", "mobile": "9800012301", "whatsapp": "919800012301" },
+            { "name": "रॉयल कट सैलून", "address": "गेट बाज़ार", "mobile": "9800012302", "whatsapp": "919800012302", "viewLink": "#", "mapLink": "https://maps.app.goo.gl/xyz2" }
+        ]
+    }
+};
